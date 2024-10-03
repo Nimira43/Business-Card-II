511ae3a723a5f3a84fe911782a61e83fb6a16bf7
Nimira43 <n.rabone72@aol.com>
2024-10-03 22:48:02 +0100

    Working on FrontCard

--- src/components/FrontCard.js
+++ src/components/FrontCard.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function FrontCard() {
   return (
-    <div>FrontCard</div>
+    <>
+      <div className="container">
+        <h1>NimiraTech</h1>
+        <p>We are a passionate team of coders, crafting innovative software solutions to drive your business forward.</p>
+        <ul>
+          <li>
+            <i className='fa-brands fa-github'></i>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <i className='fa-brands fa-discord'></i>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <i className='fa-brands fa-youtube'></i>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
