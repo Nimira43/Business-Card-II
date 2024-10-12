9a3e561cd3c0b61979b2b0d43c72cd3cf036c4ab
Nimira43 <n.rabone72@aol.com>
2024-10-12 08:47:51 +0100

    Wrote Backcard jsx

--- src/components/BackCard.js
+++ src/components/BackCard.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function BackCard() {
   return (
-    <div>BackCard</div>
+    <>
+      <div className="container">
+        <h1>Contact Us</h1>
+        <p>Email: enquiries@nimiratech.com</p>
+        <p>Phone: +44 (0)121 345 9876</p>
+      </div>
+    
+    </>
   )
 }
